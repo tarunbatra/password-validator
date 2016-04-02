724a07b2731de8d2d8fe047a42551aa9c04bd4a2
Tarun Batra <tarun.batra00@gmail.com>
2016-04-03 01:00:21 +0530

    Adds Documentation

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var _ = require('underscore');
 
+/**
+ * Generic method to test regex
+ *
+ * @params: {string} regex - regex to test
+ *                           with password
+ */
+
 var _process = function(regex) {
   if (this.valid) {
     this.valid = new RegExp(regex).test(this.password) == this.positive;
@@ -7,6 +14,11 @@ var _process = function(regex) {
   return this;
 };
 
+/**
+ * Constructor to create a password-validator object
+ *
+ * @params: {string} pwd - password to validate
+ */
 var Schema = function(pwd) {
   this.password = pwd;
   this.valid = true;
@@ -14,15 +26,24 @@ var Schema = function(pwd) {
   return this;
 };
 
+/**
+ * Method to validate the password against schema
+ */
 Schema.prototype.validate = function() {
   return this.valid;
 };
 
+/**
+ * Method to invert the next validations
+ */
 Schema.prototype.not = function() {
   this.positive = false;
   return this;
 };
 
+/**
+ * Method to invert the effects of not()
+ */
 Schema.prototype.has = function(symbol) {
   this.positive = true;
   if(symbol) {
@@ -31,6 +52,11 @@ Schema.prototype.has = function(symbol) {
   return this;
 };
 
+/**
+ * Method to specify a minimum length
+ *
+ * @params: num - minimum length
+ */
 Schema.prototype.isMin = function(num) {
   if(this.valid) {
     this.valid = this.password.length >= num;
@@ -38,6 +64,11 @@ Schema.prototype.isMin = function(num) {
   return this;
 };
 
+/**
+ * Method to specify a maximum length
+ *
+ * @params: num - maximum length
+ */
 Schema.prototype.isMax = function(num) {
   if(this.valid) {
     this.valid = this.password.length <= num;
@@ -45,26 +76,44 @@ Schema.prototype.isMax = function(num) {
   return this;
 }
 
+/**
+ * Method to validate the presense of digits
+ */
 Schema.prototype.digits = function() {
   return _process.call(this, /\d+/);
 };
 
+/**
+ * Method to validate the presense of letters
+ */
 Schema.prototype.letters = function() {
   return _process.call(this, /[a-zA-Z]+/);
 };
 
+/**
+ * Method to validate the presense of uppercase letters
+ */
 Schema.prototype.uppercase = function() {
   return _process.call(this, /[A-Z]+/);
 };
 
+/**
+ * Method to validate the presense of lowercase letters
+ */
 Schema.prototype.lowercase = function() {
   return _process.call(this, /[a-z]+/);
 };
 
+/**
+ * Method to validate the presense of symbols
+ */
 Schema.prototype.symbols = function() {
   return _process.call(this, /[`~\!@#\$%\^\&\*\(\)\-_\=\+\[\{\}\]\\\|;:'",<.>\/\?]+/);
 };
 
+/**
+ * Method to validate the presense of space
+ */
 Schema.prototype.space = function() {
   return _process.call(this, /[\s]+/);
 };
